e949a130e3398fbdb324a3af49e5f4f02c581a81
RavenLakhraj <st10296551@vcconnect.edu.za>
2025-04-07 16:27:25 +0200

    Added Dockerfile

--- CV_PROG7311_ST1029651/obj/Debug/net8.0/CV_PROG7311_ST1029651.AssemblyInfo.cs
+++ CV_PROG7311_ST1029651/obj/Debug/net8.0/CV_PROG7311_ST1029651.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CV_PROG7311_ST1029651")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffce7c7ec10a81d3935412a14d8b9e0a161b6f41")]
 [assembly: System.Reflection.AssemblyProductAttribute("CV_PROG7311_ST1029651")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CV_PROG7311_ST1029651")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
